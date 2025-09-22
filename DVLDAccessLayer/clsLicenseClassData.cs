2f9effc5057f17ac0cbc986e1324f8cc4cb695ba
Yacine Ragueb <yacineddd32@gmail.com>
2025-09-22 14:41:45 +0100

    Add LicenseClass Data Access Layer & Business Layer

---
+++ DVLDAccessLayer/clsLicenseClassData.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DVLDAccessLayer
+{
+    public class clsLicenseClassData
+    {
+        public static bool GetLicenseClassInfoByClassName(ref int LicenseClassID, string ClassName, ref string ClassDescription, ref int MinimumAllowedAge, ref int DefaultValidityLength, ref decimal ClassFees)
+        {
+            bool IsFound = false;
+            using (SqlConnection conn = new SqlConnection(clsDVLDAcessLayerSettings.connectionString))
+            {
+                string query = "SELECT * FROM LicenseClasses WHERE ClassName = @ClassName";
+
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@ClassName", ClassName);
+
+                    try
+                    {
+                        conn.Open();
+                        using (SqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+                                IsFound = true;
+
+                                LicenseClassID = (int)reader["LicenseClassID"];
+                                ClassDescription = (string)reader["ClassDescription"];
+                                MinimumAllowedAge = (int)reader["MinimumAllowedAge"];
+                                DefaultValidityLength = (int)reader["DefaultValidityLength"];
+                                ClassFees = Convert.ToDecimal(reader["ClassFees"]);
+                            }
+                            else
+                            {
+                                IsFound = false;
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        IsFound = false;
+                    }
+                }
+            }
+
+            return IsFound;
+        }
+        public static bool GetLicenseClassInfoByID( int LicenseClassID, ref string ClassName, ref string ClassDescription, ref int MinimumAllowedAge, ref int DefaultValidityLength, ref decimal ClassFees)
+        {
+            bool IsFound = false;
+            using (SqlConnection conn = new SqlConnection(clsDVLDAcessLayerSettings.connectionString))
+            {
+                string query = "SELECT * FROM LicenseClasses WHERE LicenseClassID = @LicenseClassID";
+
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@LicenseClassID", LicenseClassID);
+
+                    try
+                    {
+                        conn.Open();
+                        using (SqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+                                IsFound = true;
+
+                                ClassName = (string)reader["ClassName"];
+                                ClassDescription = (string)reader["ClassDescription"];
+                                MinimumAllowedAge = (int)reader["MinimumAllowedAge"];
+                                DefaultValidityLength = (int)reader["DefaultValidityLength"];
+                                ClassFees = Convert.ToDecimal(reader["ClassFees"]);
+                            }
+                            else
+                            {
+                                IsFound = false;
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        IsFound = false;
+                    }
+                }
+            }
+
+            return IsFound;
+        }
+        public static DataTable GetAllLicenseClasses()
+        {
+            DataTable dataTable = new DataTable();
+            using (SqlConnection conn = new SqlConnection(clsDVLDAcessLayerSettings.connectionString))
+            {
+                string query = "SELECT * FROM LicenseClasses";
+
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    try
+                    {
+                        conn.Open();
+                        using (SqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            if (reader.HasRows)
+                            {
+                                dataTable.Load(reader);
+                            }
+                        }
+                    }
+                    catch
+                    {
+
+                    }
+                }
+            }
+
+            return dataTable;
+        }
+        public static bool IsLicenseClassExistByID(int LicenseClassID)
+        {
+            bool IsFound = false;
+            using (SqlConnection connection = new SqlConnection(clsDVLDAcessLayerSettings.connectionString))
+            {
+                string query = "SELECT Found = 1 FROM LicenseClasses WHERE LicenseClassID = @LicenseClassID";
+
+                using (SqlCommand cmd = new SqlCommand(query, connection))
+                {
+                    cmd.Parameters.AddWithValue("@LicenseClassID", LicenseClassID);
+
+                    try
+                    {
+                        connection.Open();
+                        object result = cmd.ExecuteScalar();
+                        IsFound = result != null;
+                    }
+                    catch (Exception ex)
+                    {
+                        // Console.WriteLine("Error: " + ex.Message);
+                        IsFound = false;
+                    }
+                }
+            }
+
+            return IsFound;
+        }
+        public static bool IsLicenseClassExistByClassName(string ClassName)
+        {
+            bool IsFound = false;
+            using (SqlConnection connection = new SqlConnection(clsDVLDAcessLayerSettings.connectionString))
+            {
+                string query = "SELECT Found = 1 FROM LicenseClasses WHERE ClassName = @ClassName";
+
+                using (SqlCommand cmd = new SqlCommand(query, connection))
+                {
+                    cmd.Parameters.AddWithValue("@ClassName", ClassName);
+
+                    try
+                    {
+                        connection.Open();
+                        object result = cmd.ExecuteScalar();
+                        IsFound = result != null;
+                    }
+                    catch (Exception ex)
+                    {
+                        // Console.WriteLine("Error: " + ex.Message);
+                        IsFound = false;
+                    }
+                }
+            }
+
+            return IsFound;
+        }
+    }
+}
---
+++ DVLDBusinessLayer/clsLicenseClasses.cs
@@ -0,0 +1,88 @@
+﻿using ContactsDataAccessLayer;
+using DVLDAccessLayer;
+using System;
+using System.Data;
+
+namespace DVLDBusinessLayer
+{
+    public class clsLicenseClasses
+    {
+        public enum enMode { AddNew = 0, Update = 1 };
+        public enMode Mode = enMode.AddNew;
+
+        public int LicenseClassID { get; set; }
+        public string ClassName { get; set; }
+        public string ClassDescription { get; set; }
+        public int MinimumAllowedAge { get; set; }
+        public int DefaultValidityLength { get; set; }
+        public decimal ClassFees { get; set; }
+
+        public clsLicenseClasses()
+        {
+            LicenseClassID = -1;
+            ClassName = "";
+            ClassDescription = "";
+            MinimumAllowedAge = 0;
+            DefaultValidityLength = 0;
+            ClassFees = 0;
+            Mode = enMode.AddNew;
+        }
+
+        private clsLicenseClasses(int LicenseClassID, string ClassName, string ClassDescription, int MinimumAllowedAge, int DefaultValidityLength, decimal ClassFees)
+        {
+            this.LicenseClassID = LicenseClassID;
+            this.ClassName = ClassName;
+            this.ClassDescription = ClassDescription;
+            this.MinimumAllowedAge = MinimumAllowedAge;
+            this.DefaultValidityLength = DefaultValidityLength;
+            this.ClassFees = ClassFees;
+
+            Mode = enMode.Update;
+        }
+
+        public static clsLicenseClasses Find(int LicenseClassID)
+        {
+            string ClassName = "";
+            string ClassDescription = "";
+            int MinimumAllowedAge = 0;
+            int DefaultValidityLength = 0;
+            decimal ClassFees = 0;
+
+            if (clsLicenseClassData.GetLicenseClassInfoByID(LicenseClassID, ref ClassName, ref ClassDescription, ref MinimumAllowedAge, ref DefaultValidityLength, ref ClassFees))
+
+                return new clsLicenseClasses(LicenseClassID, ClassName, ClassDescription, MinimumAllowedAge, DefaultValidityLength, ClassFees);
+            else
+                return null;
+        }
+
+        public static clsLicenseClasses Find(string ClassName)
+        {
+            int LicenseClassID = -1;
+            string ClassDescription = "";
+            int MinimumAllowedAge = 0;
+            int DefaultValidityLength = 0;
+            decimal ClassFees = 0;
+
+            if (clsLicenseClassData.GetLicenseClassInfoByClassName(ref LicenseClassID, ClassName, ref ClassDescription, ref MinimumAllowedAge, ref DefaultValidityLength, ref ClassFees))
+
+                return new clsLicenseClasses(LicenseClassID, ClassName, ClassDescription, MinimumAllowedAge, DefaultValidityLength, ClassFees);
+            else
+                return null;
+        }
+
+        public static DataTable GetAllLicenseClasses()
+        {
+            return clsLicenseClassData.GetAllLicenseClasses();
+        }
+
+        public static bool IsLicenseClassExistByID(int ID)
+        {
+            return clsLicenseClassData.IsLicenseClassExistByID(ID);
+        }
+
+        public static bool IsLicenseClassExistByClassName(string ClassName)
+        {
+            return clsLicenseClassData.IsLicenseClassExistByClassName(ClassName);
+        }
+    }
+}
